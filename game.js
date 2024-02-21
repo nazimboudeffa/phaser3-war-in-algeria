0489b4f15c1fe39dd0bf96a8ee2293eb10ce04b9
Nazim Boudeffa <boudeffa@gmail.com>
2024-02-21 12:47:38 +0100

    first commit

---
+++ game.js
@@ -0,0 +1,63 @@
+class LoadScene extends Phaser.Scene
+{
+    constructor()
+    {
+        super({ key: 'load', active: false });
+    }
+
+    preload ()
+    {
+        this.load.image('algeria', 'assets/algeria.png');
+
+        let loadingBar = this.add.graphics({
+            fillStyle: {
+                color: 0xffffff //white
+            }
+        });
+
+        this.load.on("progress", (percent) => {
+            loadingBar.fillRect(0, this.game.renderer.width / 2, this.game.renderer.height * percent, 50);
+            console.log(percent);
+        })
+
+        this.load.on("complete", () => {
+            console.log("complete");
+        });
+
+        this.load.on("load", (file) => {
+            console.log(file.src)
+        })
+    }
+
+    create ()
+    {
+        this.scene.start('play');
+    }
+}
+
+class PlayScene extends Phaser.Scene
+{
+    constructor()
+    {
+        super({ key: 'play', active: false });
+    }
+
+    create ()
+    {
+        this.add.image(0, 0, 'algeria').setOrigin(0, 0);
+    }
+}
+
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser3-war-in-algeria',
+    width: 720,
+    height: 1280,
+    scene: [
+        LoadScene, PlayScene
+    ]
+};
+
+const game = new Phaser.Game(config);
+game.scene.start('load');
