8b62bf995416df323162b99d5df220ad145e3256
Nazim Boudeffa <boudeffa@gmail.com>
2024-02-25 17:17:52 +0100

    Added game scenes

--- game.js
+++ game.js
@@ -8,10 +8,16 @@ class LoadScene extends Phaser.Scene
     preload ()
     {
         this.load.image('algeria', 'assets/algeria.png');
-        this.load.image('location', 'assets/location.png');
+        this.load.image('algeria2', 'assets/carte-algerie-transformed.png');
         this.load.image('algiers', 'assets/algiers-transformed.png');
         this.load.image('eye', 'assets/eye.png');
+        this.load.image('eye-plain', 'assets/eye-plain.png');
+        this.load.image('play', 'assets/play.png');
+        this.load.image('play-button', 'assets/play-button.png');
+        this.load.image('casino', 'assets/casino.png');
+        this.load.image('milkbar', 'assets/milkbar.png');
         this.load.video('milkbar-explosion', 'assets/milkbar-explosion.mp4');
+        this.load.video('attentat-casino', 'assets/attentat-casino-cut.mp4');
 
         let loadingBar = this.add.graphics({
             fillStyle: {
@@ -61,9 +67,9 @@ class PlayScene extends Phaser.Scene
 
     create ()
     {
-        this.add.image(0, 0, 'algeria').setOrigin(0, 0);
+        this.add.image(0, 0, 'algeria2').setOrigin(0, 0);
 
-        const location = this.add.sprite(436, 13, 'location').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
+        const location = this.add.sprite(352, 90, 'eye').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
 
         location.on('pointerdown', () =>
         {
@@ -89,6 +95,10 @@ class MapScene extends Phaser.Scene
 
         const eye2 = this.add.sprite(710, 662, 'eye').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
 
+        const eye3 = this.add.sprite(669, 104, 'eye').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
+
+        
+
         let cam = this.cameras.main;
 
         this.input.on("pointermove", function (p) {
@@ -105,12 +115,79 @@ class MapScene extends Phaser.Scene
 
         eye2.on('pointerdown', () =>
         {
-            const intro = this.add.video(710, 662, 'milkbar-explosion');
+            this.scene.start('gamescenemilkbar');
+        });
+
+        eye3.on('pointerdown', () =>
+        {
+            this.scene.start('gamescenecorniche');
+        });
+    }
+}
+
+class GameSceneCorniche extends Phaser.Scene
+{
+    constructor()
+    {
+        super({ key: 'gamescenecorniche', active: false });
+    }
+
+    create ()
+    {
+        this.add.image(10, 10, 'casino').setOrigin(0, 0);
+        
+        const playvid = this.add.sprite(500, 10, 'play').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
+        const eye = this.add.sprite(500, 300, 'eye-plain').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
+
+        this.add.sprite(500, 400, 'play-button').setOrigin(0, 0);
+
+        playvid.on('pointerdown', () =>
+        {
+            const intro = this.add.video(10, 10, 'attentat-casino').setOrigin(0, 0);
             intro.play();
             intro.on('complete', () => {
                 intro.destroy();
             });
         });
+
+        eye.on('pointerdown', () =>
+        {
+            this.scene.start('map');
+        });
+        
+    }
+}
+
+class GameSceneMilkBar extends Phaser.Scene
+{
+    constructor()
+    {
+        super({ key: 'gamescenemilkbar', active: false });
+    }
+
+    create ()
+    {
+        this.add.image(10, 10, 'milkbar').setOrigin(0, 0);
+        
+        const playvid = this.add.sprite(500, 10, 'play').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
+        const eye = this.add.sprite(500, 300, 'eye-plain').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
+
+        this.add.sprite(500, 400, 'play-button').setOrigin(0, 0);
+
+        playvid.on('pointerdown', () =>
+        {
+            const intro = this.add.video(10, 50, 'milkbar-explosion').setOrigin(0, 0);
+            intro.play();
+            intro.on('complete', () => {
+                intro.destroy();
+            });
+        });
+
+        eye.on('pointerdown', () =>
+        {
+            this.scene.start('map');
+        });
+        
     }
 }
 
@@ -120,7 +197,7 @@ const config = {
     width: 800,
     height: 600,
     scene: [
-        LoadScene, PlayScene, MapScene
+        LoadScene, PlayScene, MapScene, GameSceneCorniche, GameSceneMilkBar
     ]
 };
 
